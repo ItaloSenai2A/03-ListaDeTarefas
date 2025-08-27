9390c5e487c673a53f3ec2b1cf12608f6e214f83
Ítalo <italooo.senai@gmail.com>
2025-08-27 10:36:16 -0300

    Add task management UI components and update app name

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* Cabeçalho */}
+      <View style={styles.topBar}>
+        <Text style={styles.topBarTitle}>Minhas Tarefas</Text>
+        <TouchableOpacity>
+          <Text>🌛</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Local onde o usuário insere as tarefas */}
+      <View style={styles.card}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicionar nova tarefa..."
+        />
+        <TouchableOpacity style={styles.addButton}>
+          <Text style={styles.buttonText}>Adicionar</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Lista de tarefas do usuário */}
+      <FlatList
+        style={styles.flatList}
+        ListEmptyComponent={() => (
+          <Text style={styles.emptyListText}>
+            Nenhuma tarefa adicionada ainda.
+          </Text>
+        )}
+        contentContainerStyle={styles.flatListContent}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +48,105 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e0f7fa",
+  },
+  topBar: {
+    backgroundColor:"#fff",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingTop: 50, //Ajuste para a barra de status
+    borderBottomWidth: 1,
+    borderBlockColor: "rgba(0,0,0,0.1)",
+  },
+  topBarTitle: {
+    color:"#00796b",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  card: {
+    backgroundColor:"#fff",
+    color:"#000",
+    shadowColor: "#000",
+    margin: 20,
+    borderRadius: 15,
+    padding: 20,
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 8,
+    elevation: 10,
+  },
+  input: {
+    backgroundColor:"#fcfcfc",
+    color:"#333",
+    borderColor:"#b0bec5",
+    borderWidth: 1,
+    borderRadius: 15,
+    padding: 20,
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  addButton: {
+    backgroundColor:"#009688",
+    paddingVertical: 12,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  flatListContent: {
+    paddingBottom: 10, //Espaçamento no final da Lista
+  },
+  taskItem: {
+    backgroundColor:"#fff",
+    color:"#333",
+    borderColor:"rgba(0,0,0,0.1)",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderRadius: 10,
+    padding: 15,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+    elevation: 5,
+    backgroundColor: "#fff",
+    color: "#333",
+    borderWidth: 1,
+    borderColor: "rgba(0,0,0,0.1)",
+  },
+  taskTextContainer: {
+    flex: 1, //Permite que o texto ocupe o espaço disponível
+    marginRight: 10,
+  },
+  taskText: {
+    color:"#333",
+    fontSize: 18,
+    flexWrap: "wrap", //Permite que o texto quebre linha
+  },
+  completedTaskItem: {
+    textDecorationLine: "line-through", //Risca o texto
+    opacity: 0.6,
+  },
+  deleteButton: {
+    padding: 8,
+    borderRadius: 5,
+  },
+  deleteButtonText: {
+    // color"#fff",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  emptyListText: {
+    color:"#9e9e9e",
+    textAlign: "center",
+    marginTop: 50,
+    fontSize: 16,
   },
 });
